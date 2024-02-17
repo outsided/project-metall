f69c2e81faf7c0e09299b684b21cf52349484c9f
Eduard <>
2024-02-17 21:16:57 +0100

    made icon component

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import path from 'path';
 import react from '@vitejs/plugin-react-swc'
-
+// eslint-disable-next-line import/order
+import path from 'path'
 import { defineConfig } from 'vite'
-
+import svgr from 'vite-plugin-svgr'
 
 //eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	}
 })
