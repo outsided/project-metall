e3c1d9fd28c60d95e8801a3c92ca0f729ed44dad
Olesya <oshurapej@gmail.com>
2024-02-19 21:52:38 +0300

    review fixes

--- src/widgets/header/index.ts
+++
@@ -1,1 +0,0 @@
-export { HeaderNotificationItem } from './ui'
--- src/widgets/header/ui/header-notification-item/HeaderNotificationItem.tsx
+++ src/widgets/header/ui/header-notification-item/HeaderNotificationItem.tsx
@@ -1,38 +1,44 @@
-import cl from './HeaderNotificationItem.module.scss'
+import { Typography } from '@/shared'
 
-export type THeaderNotificationItemProps = {
+import cl from './HeaderNotificationItem.module.scss'
+import userImage from './user-image.png'
+
+const { Text, Title } = Typography
+export const HeaderNotificationItem = ({
+	name,
+	description,
+	date
+}: {
 	name: string
 	description: string
-	date: string //not sure if we use string type here instead of date
-}
-
-const HARDCODED_USER_IMAGE =
-	'https://s3-alpha-sig.figma.com/img/1fc4/add1/131d549172658e1725fe4fd6b2139296?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZqvfrkGadbY~8bDBBXdxvL46jETLK2vlM7BJnKTAbjOW3zIC0jt0FGD6qNbWDIufeLnAyb3pgIq1aXEJQMY4OwdL6hE~Gm4-69EPC8J2guH2ZIIfkSSWF-2mq63i7Vz2OqeFKd-~PhBf~MiFBjiVT6MdpwX-no-DzrURV54PzfMehi67G4s7R0opCu7k99N7kNSSOpgWJrWimGTLETD0Tt5goWLAWYoH89foXI1qlIDCOe3R18f4njupLaauZ-K2PGA3hKl1DxuzYF5DoUy-w9M9~7rPXEakUCFl~2pFy2v6MomxTFrb~n7GTBCZE82btuJoLsRQAYz8GLu04-zSJg__'
-// hardcoded profile image as there's no image prop yet
-
-export const HeaderNotificationItem: React.FC<
-	THeaderNotificationItemProps
-> = props => {
-	const { name, description, date } = props
-
+	date: string
+}) => {
 	return (
 		<div className={cl.root}>
 			<div>
 				<img
 					className={cl.root__user__profile_image}
-					src={HARDCODED_USER_IMAGE}
+					src={userImage}
 					alt="profile_image"
 				/>
 			</div>
-			<div className={cl.root__notification_info}>
+			<div>
 				<div className={cl.root__notification_header}>
-					<span className={cl.root__user_name}> {name} </span>
-					<span className={cl.root__notification_description}>
-						{description}
-					</span>
+					<Title
+						variant="h3"
+						text={name}
+						className={cl.root__user_name}
+					/>
+					<Text
+						className={cl.root__notification_description}
+						text={description}
+					/>
 				</div>
 				<div>
-					<span className={cl.root__notification_date}>{date}</span>
+					<Text
+						className={cl.root__notification_date}
+						text={date}
+					/>
 				</div>
 			</div>
 		</div>
