beb15367d11a57c748abeef9b19ed165eb1e7f3a
andreyfeel83 <andreyfeel83@gmail.com>
2024-02-20 18:14:13 +0200

    fixed bugs

--- src/entities/course/ui/course-icon/CourseIcon.tsx
+++ src/entities/course/ui/course-icon/CourseIcon.tsx
@@ -1,12 +1,12 @@
 import { classNames } from '@/shared'
-import {type IconName, Icon } from '@/shared'
+import {Icon } from '@/shared'
 import cl from './CourseIcon.module.scss'
 
 
-export const CourseIcon = ({className, color, name }: {className:string, color: string, name: IconName}) => {
+export const CourseIcon = ({className, color,}: {className?:string, color: string}) => {
   return (
-    <div style={{ backgroundColor: color }} className={classNames(cl.root, className)}>
-      <Icon name={name} className={cl.root__icon} />
+    <div style={{ backgroundColor: color }} className={classNames(cl.root, className?? '')}>
+      <Icon name='Course' className={cl.root__icon} />
     </div>
   )
 }
