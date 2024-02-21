cc0d10dba44331e36f4a241b1b6e30f6caef6d8d
andreyfeel83 <andreyfeel83@gmail.com>
2024-02-21 16:18:46 +0200

    add checkbox

---
+++ src/shared/ui/checkbox/Checkbox.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox } from './Checkbox'
+
+const meta = {
+	title: 'shared/Checkbox',
+	component: Checkbox,
+	parameters: {
+		layout: 'centered'
+	},
+    tags: ['autodocs'],
+    argTypes: {
+		checked: {
+			type: 'boolean',
+			description: 'checkbox by default'
+		}
+    }
+	
+} satisfies Meta<typeof Checkbox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    
+	args: {
+        checked: false,
+	}
+}
+
+export const Checked: Story = {
+    
+	args: {
+        checked: true,
+	}
+}
